211f4498e0ac191a167cc95af7e2c501d6a8f4ca
Tân <leetaan1902@gmail.com>
2024-09-28 14:27:35 +0700

    add respository and service

--- server/yoga-business-management-web/src/main/java/org/example/yogabusinessmanagementweb/authentication/repositories/TempRepository.java
+++ server/yoga-business-management-web/src/main/java/org/example/yogabusinessmanagementweb/authentication/repositories/TempRepository.java
@@ -3,7 +3,9 @@ package org.example.yogabusinessmanagementweb.authentication.repositories;
 import org.example.yogabusinessmanagementweb.common.entities.Temp;
 import org.example.yogabusinessmanagementweb.common.entities.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TempRepository extends JpaRepository<Temp, Long> {
 
 }

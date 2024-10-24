5c8b5b035243d5c7f0a50d8031ae6927fb502124
Tân <116615872+taannnleee@users.noreply.github.com>
2024-10-25 00:40:23 +0700

    checkout (#39)

--- server/yoga-business-management-web/src/main/java/org/example/yogabusinessmanagementweb/service/OrderService.java
+++ server/yoga-business-management-web/src/main/java/org/example/yogabusinessmanagementweb/service/OrderService.java
@@ -1,8 +1,9 @@
 package org.example.yogabusinessmanagementweb.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.example.yogabusinessmanagementweb.dto.request.order.OrderCreationRequest;
 import org.example.yogabusinessmanagementweb.dto.response.order.OrderCreationResponse;
 
 public interface OrderService {
-    OrderCreationResponse createOrder(OrderCreationRequest orderCreationRequest);
+    OrderCreationResponse createOrder(HttpServletRequest request, OrderCreationRequest orderRequest);
 }

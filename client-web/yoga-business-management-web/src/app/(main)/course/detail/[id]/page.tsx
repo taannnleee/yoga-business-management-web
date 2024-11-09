a1bbf357869967493dbb1fc48d517fd6142156c5
Quang9119 <nguyenminhquangquangquang@gmail.com>
2024-11-09 19:34:32 +0700

    fix UI introvideo and add duration minutes

--- client-web/yoga-business-management-web/src/app/(main)/course/detail/[id]/page.tsx
+++ client-web/yoga-business-management-web/src/app/(main)/course/detail/[id]/page.tsx
@@ -5,6 +5,7 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import CourseContent from "@/components/organisms/CourseContent";
 import { title } from "process";
+import Video from "next-video";
 
 interface Section {
     id: number;
@@ -171,25 +172,17 @@ const CourseDetailPage: React.FC = () => {
                         <p className="mb-4">{course.instruction}</p>
                     </div>
                     <div className="lg:w-1/2 flex flex-col items-center">
-                        <div
-                            className="ytp-cued-thumbnail-overlay relative w-full h-[312px] bg-cover bg-center cursor-pointer"
-                            style={{ backgroundImage: `url(${course.imagePath})` }}
-                            onClick={() => window.open("https://www.youtube.com/watch?v=xGMXPky1wUc", "_blank")}
-                        >
-                            <button
-                                className="absolute mx-auto my-auto inset-0 flex items-center justify-center ytp-large-play-button ytp-button w-[68px] h-[48px]"
-                                aria-label="Phát"
-                                title="Phát"
-                            >
-                                <svg height="100%" width="100%" viewBox="0 0 68 48">
-                                    <path
-                                        className="ytp-large-play-button-bg"
-                                        d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"
-                                        fill="#f00"
-                                    ></path>
-                                    <path d="M 45,24 27,14 27,34" fill="#fff"></path>
-                                </svg>
-                            </button>
+                        <div>
+                            <div className="w-full max-w-[750px]">
+                                {/* Top Black Line */}
+                                <div className="border-t-2 border-black-500"/>
+                                <Video src={course.videoPath} className="w-full rounded-lg shadow-lg"/>
+                                {/* Bottom Black Line */}
+                                <div className="border-b-2 border-black-500"/>
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className="text-lg font-semibold mt-4">Video giới thiệu</h3>
                         </div>
                     </div>
                 </div>

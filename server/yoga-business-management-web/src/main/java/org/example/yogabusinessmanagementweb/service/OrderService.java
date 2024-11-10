9a83275c83f3ee4cfd00dc624eda6d10680a61fe
Quang9119 <125384623+Quang9119@users.noreply.github.com>
2024-11-10 15:05:46 +0700

    done api order (#56)
    Co-authored-by: Tân <leetaan1902@gmail.com>

--- server/yoga-business-management-web/src/main/java/org/example/yogabusinessmanagementweb/service/OrderService.java
+++ server/yoga-business-management-web/src/main/java/org/example/yogabusinessmanagementweb/service/OrderService.java
@@ -1,12 +1,15 @@
 package org.example.yogabusinessmanagementweb.service;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.example.yogabusinessmanagementweb.common.entities.Order;
 import org.example.yogabusinessmanagementweb.dto.request.order.OrderCreationRequest;
 import org.example.yogabusinessmanagementweb.dto.response.order.OrderCreationResponse;
 import org.example.yogabusinessmanagementweb.dto.response.order.OrderResponse;
 
+import java.util.List;
+
 public interface OrderService {
     OrderCreationResponse createOrder(HttpServletRequest request, OrderCreationRequest orderRequest);
 
-    OrderResponse showOrder(HttpServletRequest request);
+    List<Order> showOrder(HttpServletRequest request);
 }

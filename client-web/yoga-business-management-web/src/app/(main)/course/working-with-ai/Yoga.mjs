cafb0d757ebe4fe5db44ca186c98713994d1a736
Quang9119 <nguyenminhquangquangquang@gmail.com>
2025-03-01 15:41:02 +0700

    update: model ML

--- client-web/yoga-business-management-web/src/app/(main)/course/working-with-ai/Yoga.mjs
+++ client-web/yoga-business-management-web/src/app/(main)/course/working-with-ai/Yoga.mjs
@@ -135,7 +135,7 @@ function Yoga() {
   const runMovenet = async () => {
     const detectorConfig = { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER };
     const detector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, detectorConfig);
-    const poseClassifier = await tf.loadLayersModel('https://models.s3.jp-tok.cloud-object-storage.appdomain.cloud/model.json')
+    const poseClassifier = await tf.loadLayersModel('/model-ml/model.json');
     const countAudio = new Audio('/music/count.wav');
     countAudio.loop = true
     interval = setInterval(() => {

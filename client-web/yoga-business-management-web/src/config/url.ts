3d379ceef9460503c5c9abd564b8cf69644153e4
leetaan1902@gmail.com <leetaan1902@gmail.com>
2025-04-03 20:07:47 +0700

    deploy finish

--- client-web/yoga-business-management-web/src/config/url.ts
+++ client-web/yoga-business-management-web/src/config/url.ts
@@ -1,4 +1,6 @@
 // src/config/config.tsx
-export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://localhost:8080";
 
 
+//deployment
+export const API_URL = "https://yoga-business-management-web-3.onrender.com";
